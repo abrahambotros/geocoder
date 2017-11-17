86e39454c2dceb935aebafb3fe0d528a0f727672
abrahambotros <abrahambotros@gmail.com>
2017-11-17 09:59:02 -0800

    Add main controller, stub 3rd-party controllers

---
+++ app/controllers/main.py
@@ -0,0 +1,23 @@
+"""
+Main controller providing a simple interface for API requests to geocode based
+on an input address string. Abstracts away callers from having to interact with
+third-party geocoding service requests and backup/fallback logic.
+"""
+
+# External imports
+# - N/A
+
+# Internal imports
+# - N/A
+
+
+def geocode(address: str) -> str:
+    """
+    Geocode the given address string to (lat, lng) coordinates.
+
+    TODO: Implement.
+    TODO: Document.
+    TODO: Adjust return type.
+    """
+    # TODO: Return appropriate data, not just input address.
+    return address

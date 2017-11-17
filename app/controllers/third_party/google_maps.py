fab111a7890af5baf93c902fbdc4b0ff1b98c381
abrahambotros <abrahambotros@gmail.com>
2017-11-17 15:41:11 -0800

    Implement request to gmaps API; print result
    * Use urllib to make GET request to Google Maps Geocoding API, including
      encoding URL params for address and API key.
    * Note that SSL verification needs to be disabled due to strict Python 3
      SSL constraints. Add TODO to fix later.
    * Simply print result to console at the moment.

--- app/controllers/third_party/google_maps.py
+++ app/controllers/third_party/google_maps.py
@@ -4,10 +4,20 @@ API.
 """
 
 # External imports
-# - N/A
+import json
+import ssl
+import urllib.request
+from urllib.parse import urlencode
+from typing import Dict
 
 # Internal imports
 from app.models.lat_lng import LatLng
+from app.conf.settings import GMAPS_API_KEY
+
+# Constants
+_URL_GEOCODE_BASE = "https://maps.googleapis.com/maps/api/geocode/json"
+_URL_PARAM_ADDRESS = "address"
+_URL_PARAM_API_KEY = "key"
 
 
 def geocode(address: str) -> LatLng:
@@ -20,12 +30,28 @@ def geocode(address: str) -> LatLng:
     """
     # Build URL params dict for request, with address string and Google Maps API
     # key from settings.
+    params_dict: Dict[str, str] = {
+        _URL_PARAM_ADDRESS: address,
+        _URL_PARAM_API_KEY: GMAPS_API_KEY,
+    }
 
     # URL-encode the URL params dict into a URL-encoded string.
+    params_str: str = urlencode(params_dict)
 
-    # Fire off the request to the Google Maps Geocoding API.
+    # Fire off the request to the Google Maps Geocoding API. Parse response to
+    # JSON dict.
+    url = "%s?%s" % (_URL_GEOCODE_BASE, params_str)
+    # TODO: Python 3's increased SSL security for requests will likely cause a
+    #    CERTIFICATE_VERIFY_FAILED error on most local systems without elaborate
+    #    certificate setup. Because of this, we currently disable SSL
+    #    verification of the CONTROLLED requests we make to the known API
+    #    endpoints via the explicit context setting below.
+    context = ssl._create_unverified_context()
+    with urllib.request.urlopen(url=url, context=context) as resp:
+        resp_data = json.loads(resp.read().decode("utf-8"))
 
-    # Parse response latitude and longitude into LatLng instance.
+    # Parse response data latitude and longitude into LatLng instance.
+    print(resp_data, type(resp_data))
 
     # Return created LatLng instance with success.
     # TODO: Use real instance instead of placeholder.

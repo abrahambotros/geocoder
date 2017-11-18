c0f1b87a30cac66eae869f244726ae8d01375e16
abrahambotros <abrahambotros@gmail.com>
2017-11-17 21:25:27 -0800

    Note specific exceptions thrown by urllib, json

--- app/controllers/third_party/request.py
+++ app/controllers/third_party/request.py
@@ -27,19 +27,23 @@ def make_geocode_request(base_url: str, params_dict: Dict[str, str]) -> Dict:
     Returns:
         Dictionary representing the JSON response from the third-party geocoding
             API service.
+
+    Raises:
+        urllib.error.URLError: If error issuing request to external endpoint.
+        json.JSONDecodeError: If error decoding the response string to a JSON
+            object.
     """
     # URL-encode the URL params dict into a URL-encoded string.
     params_str: str = urlencode(params_dict)
 
     # Fire off the request to the base URL, with URL params attached. Parse
-    # response to JSON dict.
+    # response to JSON dict. Raised exceptions noted in docstring above.
     url = "%s?%s" % (base_url, params_str)
     # TODO: Python 3's increased SSL security for requests will likely cause a
     #    CERTIFICATE_VERIFY_FAILED error on most local systems without elaborate
     #    certificate setup. Because of this, we currently disable SSL
     #    verification of the CONTROLLED requests we make to the known API
     #    endpoints via the explicit context setting below.
-    # TODO: Handle errors.
     context = ssl._create_unverified_context()
     with urllib.request.urlopen(url=url, context=context) as resp:
         return json.loads(resp.read().decode("utf-8"))

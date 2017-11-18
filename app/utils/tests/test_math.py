ee3cc47a3a51eb977c3b38f111f5f96d1ef129d1
abrahambotros <abrahambotros@gmail.com>
2017-11-17 17:25:18 -0800

    Fix test cases: precision fix

--- app/utils/tests/test_math.py
+++ app/utils/tests/test_math.py
@@ -15,5 +15,7 @@ class TestMath(unittest.TestCase):
         equalish = utils_math.equalish
         self.assertTrue(equalish(0, 0))
         self.assertFalse(equalish(0, 1))
-        self.assertTrue(equalish(x=0.55555, y=0.55556, precision_digits=6))
+        self.assertTrue(equalish(x=0.55555, y=0.55556, precision_digits=3))
+        self.assertTrue(equalish(x=0.55555, y=0.55556, precision_digits=4))
         self.assertFalse(equalish(x=0.55555, y=0.55556, precision_digits=5))
+        self.assertFalse(equalish(x=0.55555, y=0.55556, precision_digits=6))

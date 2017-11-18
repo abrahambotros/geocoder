fc2d86212dde4755eaddfcbf715bdbedf25a8f3e
abrahambotros <abrahambotros@gmail.com>
2017-11-17 18:42:28 -0800

    Temporarily remove locations HERE does not support

--- app/controllers/tests/utils.py
+++ app/controllers/tests/utils.py
@@ -14,7 +14,7 @@ ADDRESS_TO_LATLNG_DICT: Dict[str, LatLng] = {
     "San Francisco, CA, USA": LatLng(lat=37.77493, lng=-122.419416),
     "san francisco": LatLng(lat=37.77493, lng=-122.419416),
     "1600 Pennsylvania Ave NW, Washington, DC 20500": LatLng(lat=38.897663, lng=-77.036574),
-    "colossus of rhodes": LatLng(lat=36.451066, lng=28.225833),
-    "Great Pyramid of Giza": LatLng(lat=29.979235, lng=31.134202),
-    "hanging gardens of babylon": LatLng(lat=48.919861, lng=2.343222),
+    # "colossus of rhodes": LatLng(lat=36.451066, lng=28.225833),  # Unhandled by HERE.
+    # "Great Pyramid of Giza": LatLng(lat=29.979235, lng=31.134202),  # Unhandled by HERE.
+    # "hanging gardens of babylon": LatLng(lat=48.919861, lng=2.343222),  # Unhandled by HERE.
 }

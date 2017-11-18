5acc822f948485d3949db34c8c965b9bf532b24a
abrahambotros <abrahambotros@gmail.com>
2017-11-17 17:07:23 -0800

    Implement parsing geocoding results to LatLng

--- app/controllers/third_party/google_maps.py
+++ app/controllers/third_party/google_maps.py
@@ -13,6 +13,7 @@ from typing import Dict
 # Internal imports
 from app.models.lat_lng import LatLng
 from app.conf.settings import GMAPS_API_KEY
+from app.utils import dictionary as utils_dictionary
 
 # Constants
 _URL_GEOCODE_BASE = "https://maps.googleapis.com/maps/api/geocode/json"
@@ -46,13 +47,46 @@ def geocode(address: str) -> LatLng:
     #    certificate setup. Because of this, we currently disable SSL
     #    verification of the CONTROLLED requests we make to the known API
     #    endpoints via the explicit context setting below.
+    # TODO: Handle errors.
     context = ssl._create_unverified_context()
     with urllib.request.urlopen(url=url, context=context) as resp:
-        resp_data = json.loads(resp.read().decode("utf-8"))
+        resp_dict = json.loads(resp.read().decode("utf-8"))
 
-    # Parse response data latitude and longitude into LatLng instance.
-    print(resp_data, type(resp_data))
+    # Parse response data dict's latitude and longitude into LatLng instance and
+    # return with success. If error, raise exception.
+    try:
+        return _parse_geocode_response_dict_to_lat_lng(resp_dict)
+    except Exception:
+        raise RuntimeError("Error parsing Google Maps Geocoding API response")
 
-    # Return created LatLng instance with success.
-    # TODO: Use real instance instead of placeholder.
-    return LatLng(lat=0, lng=0)
+
+def _parse_geocode_response_dict_to_lat_lng(d: Dict) -> LatLng:
+    """
+    TODO: Document. Raises exception.
+    """
+    # If invalid input, raise error.
+    if not d:
+        raise ValueError("Invalid empty input dict")
+
+    # Get top-level result from results list.
+    results = d["results"]
+    if not results:
+        raise ValueError("Invalid empty results list")
+    result: Dict = results[0]
+
+    # Get results->geometry->location subdict.
+    location_key_path = ["geometry", "location"]
+    location: Dict = utils_dictionary.get_nested_value(
+        dictionary=result,
+        keys=location_key_path,
+        default={},
+    )
+
+    # Get latitude and longitude from location subdict.
+    lat = location.get("lat", None)
+    lng = location.get("lng", None)
+    if not lat or not lng:
+        raise ValueError("Unable to parse valid (lat, lng) from results")
+
+    # Return corresponding LatLng model instance.
+    return LatLng(lat=lat, lng=lng)

6062f194cf8fcad5a0e0e6b26580f8f52d4d96a5
abrahambotros <abrahambotros@gmail.com>
2017-11-17 18:07:18 -0800

    Implement parsing incoming request for address
    * Expects "address" as (URL) param in incoming GET request to root
      route.

--- app/app.py
+++ app/app.py
@@ -3,11 +3,13 @@ Entry-point for this application.
 """
 
 # External modules
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 # Internal modules
 from app.controllers import main as main_controller
 
+# Constants
+_URL_PARAM_ADDRESS = "address"
 
 # Instantiate the Flask app server, once and for all.
 SERVER = Flask(__name__)
@@ -18,18 +20,15 @@ def geocode() -> str:
     """
     Root geocode response handler.
 
-    TODO: Parse address from request.
     TODO: Handle errors.
     """
     # Parse address from request.
-    # TODO: Implement.
+    address = request.args.get(_URL_PARAM_ADDRESS)
 
     # Pass address from request to main geocoding controller to get geocoded
     # LatLng response.
     try:
-        lat_lng = main_controller.geocode(
-            address="",
-        )
+        lat_lng = main_controller.geocode(address=address)
     except Exception:
         return jsonify({})
 

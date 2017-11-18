63d81d78079d09f170ed7ef4d82d0a1842f1a1dc
abrahambotros <abrahambotros@gmail.com>
2017-11-17 18:43:19 -0800

    Update gmaps geocode to match HERE improvements

--- app/controllers/third_party/google_maps.py
+++ app/controllers/third_party/google_maps.py
@@ -76,14 +76,16 @@ def _parse_geocode_response_dict_to_lat_lng(d: Dict) -> LatLng:
     location: Dict = utils_dictionary.get_nested_value(
         dictionary=result,
         keys=location_key_path,
-        default={},
+        default=None,
     )
+    if not location:
+        raise ValueError("Invalid empty location dict")
 
     # Get latitude and longitude from location subdict.
     lat = location.get("lat", None)
     lng = location.get("lng", None)
     if not lat or not lng:
-        raise ValueError("Unable to parse valid (lat, lng) from results")
+        raise ValueError("Unable to parse valid (lat, lng) from location dict")
 
     # Return corresponding LatLng model instance.
     return LatLng(lat=lat, lng=lng)
